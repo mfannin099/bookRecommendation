c135b875db2b566335cfe61f29adcd2d40912c4d
mfannin <mattfannin6@gmail.com>
2025-04-13 13:09:52 -0400

    Commenting some code

--- recommendation.py
+++ recommendation.py
@@ -28,10 +28,10 @@ BOOKS_TO_RETURN = 30 # Max is 40... Per API docs
 
 
 ## Begin the script
-final_books_df = check_to_run_initial_data_load(CACHE_PATH,titles_l,authors_l, FORCE_RUN)
-final_books_df = clean_data_for_tfidf(final_books_df,MATCH_SCORE,LAST_N_BOOKS,titles_l)
-search_query = tfidf(df=final_books_df, terms = TERMS_IN_SEARCH_QUERY)
-recs_from_google = get_book_recs_from_api(search_query=search_query,n=BOOKS_TO_RETURN)
+final_books_df = check_to_run_initial_data_load(CACHE_PATH,titles_l,authors_l, FORCE_RUN) #Checking if data pull is needed and runs if so
+final_books_df = clean_data_for_tfidf(final_books_df,MATCH_SCORE,LAST_N_BOOKS,titles_l) # Cleaning the data
+search_query = tfidf(df=final_books_df, terms = TERMS_IN_SEARCH_QUERY) # Returns optimized query for future recommendations
+recs_from_google = get_book_recs_from_api(search_query=search_query,n=BOOKS_TO_RETURN) # Retrieving the recommendations and returns a dataframe
 
 print(recs_from_google)
 
